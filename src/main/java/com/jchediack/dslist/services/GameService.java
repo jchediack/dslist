662f8e729b7991ba4833c6135099cc17cc872b1e
Jorge Chediack <jorge.logan@gmail.com>
2025-01-22 19:02:42 +0000

    GET List Games, Projections, @Query

--- src/main/java/com/jchediack/dslist/services/GameService.java
+++ src/main/java/com/jchediack/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.jchediack.dslist.services;
 import com.jchediack.dslist.dto.GameDTO;
 import com.jchediack.dslist.dto.GameMinDTO;
 import com.jchediack.dslist.entities.Game;
+import com.jchediack.dslist.projections.GameMinProjection;
 import com.jchediack.dslist.repositories.GameRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,10 @@ public class GameService {
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 
-
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
 
 }
